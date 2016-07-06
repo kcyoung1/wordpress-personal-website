52c0d42e8b95dc4c99849090ba89baa52321bf7a
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-07-06 07:48:19 -0700

    styling the error page and adding a btn class that can be extended

--- themes/kcyoung-theme/404.php
+++ themes/kcyoung-theme/404.php
@@ -18,41 +18,9 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'kcyoung-theme' ); ?></p>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Click the button to go back to the front page.', 'kcyoung-theme' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( kcyoung_theme_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'kcyoung-theme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'kcyoung-theme' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					<a href="/" class="btn">Home</a>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
