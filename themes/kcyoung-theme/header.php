95431638d992657b976cb04501351a347340e4fa
Kurtis Young <iBeast@Kurtiss-MacBook-Pro.local>
2016-07-07 11:52:50 -0700

    adding logo to browser tab and new screenshot of website

--- themes/kcyoung-theme/header.php
+++ themes/kcyoung-theme/header.php
@@ -16,6 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
 
 <?php wp_head(); ?>
 </head>
